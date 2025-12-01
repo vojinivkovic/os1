4021334f6355ba95ed075bf2bc0a4de64a2b5507
vojinii <vojinivkovic02@gmail.com>
2025-12-01 15:47:34 -0800

    Assembly for system_call

---
+++ build/src/Kernel.d
@@ -0,0 +1,8 @@
+build/src/Kernel.o: src/Kernel.cpp src/../h/Kernel.hpp \
+ src/../h/Machine.hpp src/../h/../lib/hw.h
+
+src/../h/Kernel.hpp:
+
+src/../h/Machine.hpp:
+
+src/../h/../lib/hw.h:
---
+++ build/src/Machine.d
@@ -0,0 +1,6 @@
+build/src/Machine.o: src/Machine.cpp src/../h/Machine.hpp \
+ src/../h/../lib/hw.h
+
+src/../h/Machine.hpp:
+
+src/../h/../lib/hw.h:
