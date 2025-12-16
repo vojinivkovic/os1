4e7514e441219e78e7e8d32c324643fbf4bcae31
vojinii <vojinivkovic02@gmail.com>
2025-12-16 12:48:27 -0800

    Template class Queue implemented

---
+++ h/Queue.hpp
@@ -0,0 +1,82 @@
+//
+// Created by os on 12/16/25.
+//
+
+#ifndef PROJECT_BASE_V1_1_QUEUE_H
+#define PROJECT_BASE_V1_1_QUEUE_H
+#include "../lib/hw.h"
+
+template<typename T>
+class Queue {
+public:
+    Queue() = default;
+
+    void append(T* newElement);
+    T* take();
+    void removeElement(T* element);
+
+    static void* operator new(size_t size);
+private:
+    T* head = nullptr, *tail = nullptr;
+};
+
+template<typename T>
+void Queue<T>::append(T *newElement)
+{
+    if(!head)
+    {
+        head = newElement;
+    }
+    else
+    {
+        tail->addThreadToState(newElement);
+    }
+    tail = newElement;
+}
+template<typename T>
+T* Queue<T>::take()
+{
+    T* oldElement = head;
+    head = head->getState();
+    if(!head)
+    {
+        tail = nullptr;
+    }
+    return oldElement;
+}
+
+template<typename T>
+void Queue<T>::removeElement(T *element)
+{
+    T* prev = nullptr, * curr = head;
+    while(element != curr && curr)
+    {
+        prev = curr;
+        curr = curr->getState();
+    }
+    if(!prev)
+    {
+        head = head->getState();
+        if(!head)
+        {
+            tail = nullptr;
+        }
+    }
+    else
+    {
+        prev->addThreadToState(element->getState());
+        if(element == tail)
+        {
+            tail = prev;
+        }
+    }
+}
+template<typename T>
+void* Queue<T>::operator new(size_t size)
+{
+    size_t numOfBlocks = size / MEM_BLOCK_SIZE;
+    numOfBlocks += size % MEM_BLOCK_SIZE ? 1 : 0;
+    return MemoryAllocator::allocateMemory(numOfBlocks);
+}
+
+#endif //PROJECT_BASE_V1_1_QUEUE_H
---
+++ src/Queue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by os on 12/16/25.
+//
+
+#include "../h/Queue.hpp"
